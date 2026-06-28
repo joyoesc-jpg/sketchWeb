0f8aeb656a3b2d7e37abc1571bb4753a5ffcf04f
Titos <carloscw0304@gmail.com>
2026-06-28 17:11:13 -0600

    script sql completado

--- db/db.sql
+++ db/db.sql
@@ -23,3 +23,8 @@ CREATE TABLE IF NOT EXISTS project(
 
 INSERT INTO user (userName, email, password) VALUES
 ('admin','admin@example','1234');
+
+INSERT INTO project (id, projectname, filename, user_ID) VALUES
+(20, "test", "/00021-2026-5-26", 1),
+(23, "test2", "/00023-2026-5-26", 1),
+(25, "test3", "/00025-2026-5-27", 1);
